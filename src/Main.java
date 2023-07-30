d3c43914e54d9e4bda633cdd0439546c271f1bf9
Kiramman <kiramman89@gmail.com>
2023-07-30 16:33:32 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 11540;
+        int mile = 20;
+        int bonus_miles = cost / mile;
+        System.out.println((bonus_miles) + " Bonus miles");
+    }
+}
